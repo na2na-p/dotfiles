664783aa8990e3de2d8845427969dce4c7fe6ecf
na2na-p <na2na@na2na.dev>
2023-07-25 21:54:13 +0900

    Update

---
+++ init.el
@@ -0,0 +1,99 @@
+;;; package.el
+(require 'package)
+
+;;; MELPA
+(when (require 'package nil t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+  (add-to-list 'package-archives '("maralade" . "https://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+  (package-initialize) )
+
+;; font
+(add-to-list 'default-frame-alist '(font . "Koruri"))
+
+;; color theme
+(load-theme 'monokai t)
+
+;; alpha
+(if window-system 
+    (progn
+      (set-frame-parameter nil 'alpha 95)))
+
+;; tabサイズ
+(setq default-tab-width 4)
+
+;; メニューバーを非表示
+;; (menu-bar-mode 0)
+
+;; ツールバーを非表示
+;; (tool-bar-mode 0)
+
+;; default scroll bar消去
+(scroll-bar-mode -1)
+
+;; 現在ポイントがある関数名をモードラインに表示
+(which-function-mode 1)
+
+;; 対応する括弧をハイライト
+(show-paren-mode 1)
+
+;; リージョンのハイライト
+(transient-mark-mode 1)
+
+;; タイトルにフルパス表示
+(setq frame-title-format "%f")
+
+;;current directory 表示
+(let ((ls (member 'mode-line-buffer-identification
+                  mode-line-format)))
+  (setcdr ls
+    (cons '(:eval (concat " ("
+            (abbreviate-file-name default-directory)
+            ")"))
+            (cdr ls))))
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message 1)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; ターミナルで起動したときにメニューを表示しない
+(if (eq window-system 'x)
+    (menu-bar-mode 1) (menu-bar-mode 0))
+(menu-bar-mode nil)
+
+;; scratchの初期メッセージ消去
+(setq initial-scratch-message "")
+
+;; スクロールは1行ごとに
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 5)))
+
+;; スクロールの加速をやめる
+(setq mouse-wheel-progressive-speed nil)
+
+;; bufferの最後でカーソルを動かそうとしても音をならなくする
+(defun next-line (arg)
+  (interactive "p")
+  (condition-case nil
+      (line-move arg)
+    (end-of-buffer)))
+
+;; メニューバーを消す
+;; (menu-bar-mode -1)
+
+;; エラー音をならなくする
+(setq ring-bell-function 'ignore)
+
+;; カーソルの点滅をやめる
+;; (blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
