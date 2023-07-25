1b8ac6b917c852ff03341d00a275090159a7d36a
na2na-p <na2na@na2na.dev>
2023-07-25 22:59:17 +0900

    Add magit

--- shared/.emacs.d/init.el
+++ shared/.emacs.d/init.el
@@ -97,3 +97,16 @@
 
 ;; 対応する括弧を光らせる
 (show-paren-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit cmake-mode monokai-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ (global-set-key (kbd "C-x g") 'magit-status)
