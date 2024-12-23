4ba649cf87c93a28b23896f053ae5098d391cb8e
na2na-p <na2na@na2na.dev>
2024-12-23 22:07:34 +0900

    asdf pluginの一括導入用スクリプト追加

---
+++ shared/.bin/asdf-plugin-setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# List of asdf plugins with optional URLs
+plugins=(
+  "python"
+  "pipenv https://github.com/and-semakin/asdf-pipenv.git"
+  "nodejs"
+  "jq"
+  "yq"
+  "deno"
+  "pnpm"
+  "rust"
+  "golang"
+  "kubectl"
+  "helm"
+  "kustomize"
+	"skaffold"
+  "dotnet-core"
+  "terraform"
+  "tfsec"
+  "tflint"
+  "tfcmt"
+  "awscli"
+  "terragrunt"
+  "minikube"
+  "air"
+  "flutter"
+  "okteto"
+  "kompose"
+  "ffmpeg"
+  "trivy"
+  "argocd"
+  "oci"
+  "golangci-lint"
+)
+
+# Adding plugins
+for plugin_info in "${plugins[@]}"; do
+  plugin_name=$(echo "$plugin_info" | awk '{print $1}')
+  plugin_url=$(echo "$plugin_info" | awk '{print $2}')
+
+  if asdf plugin-list | grep -q "^${plugin_name}$"; then
+    echo "[INFO] ${plugin_name} is already added."
+  else
+    echo "[INFO] Adding ${plugin_name}..."
+    if [ -n "$plugin_url" ]; then
+      # Add plugin with URL
+      if asdf plugin-add "$plugin_name" "$plugin_url"; then
+        echo "[SUCCESS] ${plugin_name} has been added from ${plugin_url}."
+      else
+        echo "[ERROR] Failed to add ${plugin_name} from ${plugin_url}."
+      fi
+    else
+      # Add plugin without URL
+      if asdf plugin-add "$plugin_name"; then
+        echo "[SUCCESS] ${plugin_name} has been added."
+      else
+        echo "[ERROR] Failed to add ${plugin_name}."
+      fi
+    fi
+  fi
+done
+
+echo "All plugin additions are complete."
